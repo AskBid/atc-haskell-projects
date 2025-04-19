3abcf71b2f6ff5d12bb00a3e73839e9610921595
AskBid <noreply>
2025-04-19 23:15:36 +0100

    adding haddock documentation comments to Task

--- src/Task.hs
+++ src/Task.hs
@@ -11,20 +11,29 @@ data Task = Task
 data Priority = High | Medium | Low
   deriving (Show, Enum)
 
+-- | Custom instantiation to keep the equality to the identity attributes only.
 instance Eq Task where
-  (Task completed name _) == (Task completed' name' _) = completed == completed' && name' == name
+  (Task completed name _) == (Task completed' name' _) = 
+    completed == completed' && name' == name
 
+-- | getTask finds a task in the todo list @[Task]@.
+--   It takes a predicate function so that the filter is customisable with various equalities.
 getTask :: (Task -> Bool) -> [Task] -> Either String Task
 getTask predicate ts = case filter predicate ts of
   []    -> Left $ "No matching task was found."
   (t:_) -> Right t
 
+-- | replaceTask replaces a task modified after finding it with getTask. 
+--   It keeps the list order while modifying the Task.
+--   It takes the old Task, the new Task to replace it with and the original todo list.
+-- >>> replaceTask oldtask (Task True (name oldtask) (description oldtask) ... ) todoList
 replaceTask :: Task -> Task -> [Task] -> [Task]
 replaceTask _ _ [] = []
 replaceTask old new (t:ts)
   | old == t  = new:ts
   | otherwise = t:(replaceTask old new ts)
 
+-- | deleteTask takes a Task to be found and deleted from a list @[Task]@
 deleteTask :: Task -> [Task] -> [Task]
 deleteTask _ [] = []
 deleteTask task (t:ts)
