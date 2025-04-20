0c223459e20d3ad805eb5036acc787eef95a4bdb
AskBid <noreply>
2025-04-20 16:05:03 +0100

    starts various parsers to check on name, completed mark and description separately

--- src/File.hs
+++ src/File.hs
@@ -14,9 +14,26 @@ writeTask t = (name t) ++
   "," ++ (show $ completed t) ++ 
   "," ++ (description t) ++ "\n"
 
+-- probably no need for a parser here as a @split ','@ would be enough..
+-- but I was willing to put to practice the Parsec library I just read on.
 parserTask :: Parser Task
 parserTask = do
   name <- many letter
   char ','
   description <- many letter
   return (Task False name description)
+
+parserTaskName :: Parser String
+parserTaskName = do
+  name <- many1 $ letter <|> digit <|> oneOf "-. "
+  return name
+
+parserDescription :: Parser String
+parserDescription = do
+  description <- many anyChar
+  return description
+
+parserCompleted :: Parser Bool
+parserCompleted = do
+  mark <- string "True" <|> string "False"
+  return $ read mark
