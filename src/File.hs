585bbf64efa33fe321e70857fb6bda13cacd3bf4
AskBid <noreply>
2025-04-20 15:43:07 +0100

    parser for task in file form (study)

--- src/File.hs
+++ src/File.hs
@@ -1,6 +1,8 @@
 module File where
 
 import System.IO
+import Text.Parsec
+import Text.Parsec.String (Parser)
 
 import Task
 
@@ -11,3 +13,10 @@ writeTask :: Task -> String
 writeTask t = (name t) ++
   "," ++ (show $ completed t) ++ 
   "," ++ (description t) ++ "\n"
+
+parserTask :: Parser Task
+parserTask = do
+  name <- many letter
+  char ','
+  description <- many letter
+  return (Task False name description)
