61da6b3820e0976c515489f7a5b38b82bc8a3884
AskBid <noreply>
2025-04-22 15:32:27 +0100

    refactored interface to change Task attributs out of handleInput

--- src/Interface.hs
+++ src/Interface.hs
@@ -20,27 +20,6 @@ loop ts = do
     then loop tsNew
     else return ()
 
--- | nameCheck uses the Parser to read from file "todo.txt" for checking CLI inputs have 
---   a compatible format. It does loop if no copatible input is given.
-nameCheck :: IO (String)
-nameCheck = do
-  name <- getLine
-  let check = parse parserTaskName "" name
-  case check of 
-    Left e -> do
-      putStrLn "Only letters, `-`, `.` and spaces are accepted for names."
-      putStrLn "Enter name again:"
-      nameCheck
-    Right n -> pure n
-
--- | processCurrentDay is used as argument for the @Parser.parserDueDate@ to get
---   today's date in Day type format, so that we can enter a nuber of days to set
---   the due date from today's date.
-processCurrentDay :: IO Day
-processCurrentDay = do
-  currentTime <- getCurrentTime
-  return $ utctDay currentTime
-
 -- | handleInput to dispatch the input command from CLI interface.
 --   all modification to the TodoList try to find the Task first and 
 --   later replace it with a modified Task.
@@ -54,8 +33,7 @@ handleInput ts "add" = do
   putStrLn "Enter task:"
   name <- nameCheck
   task <- mkTask name
-  putStrLn "new task created:"
-  putStr $ show task
+  task'<- enterTaskAttributes task
   pure (True, task:ts)
 
 handleInput ts "view" = do
@@ -97,22 +75,61 @@ handleInput ts "edit" = do
     Left err   -> do
       putStrLn err
       pure (True, ts)
-    Right task -> do
-      putStrLn "Current task description:"
-      putStrLn $ description task
-      putStrLn "Enter new name or leave empty to keep the same:"
-      nameIO <- nameCheck
-      let newName = if nameIO == "" then name task else nameIO
-      putStrLn "Enter new description or leave empty to keep the same:"
-      descriptionIO <- getLine
-      let newDescription = if descriptionIO == "" then description task else descriptionIO
-      let newTask = Task (completed task) newName (priority task) (date task) newDescription
+    Right task -> do 
+      newTask <- enterTaskAttributes task
       let newTS = replaceTask task newTask ts
-      putStrLn "Task edited:"
-      putStrLn $ show newTask
       pure (True, newTS)
 
 handleInput ts input = do
   putStrLn $ "You entered: " ++ input
   putStrLn "Not a command."
   pure (True, ts)
+
+-- | enterTaskAttributes is the part of the CLI interface used to deal with 
+--   @Task@ attributes insertion or edits.
+enterTaskAttributes :: Task -> IO Task
+enterTaskAttributes task = do  
+  putStrLn $ "Current due date: " 
+  putStrLn $ show $ date task
+  putStrLn "Enter due date (YYYY-MM-DD) or number of days for duration, leave empty to leave unchanghed"
+  putStrLn "Current task description:"
+  putStrLn $ "\"" ++ description task ++ "\""
+  putStrLn "Enter new name or leave empty to keep the same:"
+  nameIO <- nameCheck
+  let newName = 
+        if nameIO == "" 
+        then name task 
+        else nameIO
+  putStrLn "Enter new description or leave empty to keep the same:"
+  descriptionIO <- getLine
+  let newDescription = 
+        if descriptionIO == "" 
+        then description task 
+        else descriptionIO
+  let newTask = Task (completed task) newName (priority task) (date task) newDescription
+  putStrLn "Task edited/created:"
+  putStrLn $ show newTask
+  return newTask
+
+-- | nameCheck uses the Parser to read from file "todo.txt" for checking CLI inputs have 
+--   a compatible format. It does loop if no copatible input is given.
+nameCheck :: IO (String)
+nameCheck = do
+  name <- getLine
+  let check = parse parserTaskName "" name
+  case check of 
+    Left e -> do
+      putStrLn "Only letters, `-`, `.` and spaces are accepted for names."
+      putStrLn "Enter name again:"
+      nameCheck
+    Right n -> pure n
+
+-- | processCurrentDay is used as argument for the @Parser.parserDueDate@ to get
+--   today's date in Day type format, so that we can enter a nuber of days to set
+--   the due date from today's date.
+processCurrentDay :: IO Day
+processCurrentDay = do
+  currentTime <- getCurrentTime
+  return $ utctDay currentTime
+
+
