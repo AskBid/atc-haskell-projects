11ffde762ae5d2641a3a31b67bcd7c8c1f734c6e
AskBid <noreply>
2025-04-22 15:48:49 +0100

    creating a function for date interface handling only, so to make it recursive

--- src/Interface.hs
+++ src/Interface.hs
@@ -86,34 +86,36 @@ handleInput ts input = do
   pure (True, ts)
 
 -- | enterTaskAttributes is the part of the CLI interface used to deal with 
---   @Task@ attributes insertion or edits.
+--   @Task@ attributes insertion or ed1its.
 enterTaskAttributes :: Task -> IO Task
 enterTaskAttributes task = do  
-  putStrLn $ "Current due date: " 
-  putStrLn $ show $ date task
-  putStrLn "Enter due date (YYYY-MM-DD) or number of days for duration, leave empty to leave unchanghed"
-  putStrLn "Current task description:"
-  putStrLn $ "\"" ++ description task ++ "\""
   putStrLn "Enter new name or leave empty to keep the same:"
   nameIO <- nameCheck
   let newName = 
         if nameIO == "" 
         then name task 
         else nameIO
+  putStrLn $ "Current due date: " 
+  putStrLn $ show $ date task
+  putStrLn "Enter due date (YYYY-MM-DD) or number of days from today" 
+  putStrLn "(press Enter to leave unchanghed):"
+  newDate <- dateCheck
+  putStrLn "Current task description:"
+  putStrLn $ "\"" ++ description task ++ "\""
   putStrLn "Enter new description or leave empty to keep the same:"
   descriptionIO <- getLine
   let newDescription = 
         if descriptionIO == "" 
         then description task 
         else descriptionIO
-  let newTask = Task (completed task) newName (priority task) (date task) newDescription
+  let newTask = Task (completed task) newName (priority task) (date newDate) newDescription
   putStrLn "Task edited/created:"
   putStrLn $ show newTask
   return newTask
 
 -- | nameCheck uses the Parser to read from file "todo.txt" for checking CLI inputs have 
 --   a compatible format. It does loop if no copatible input is given.
-nameCheck :: IO (String)
+nameCheck :: IO String
 nameCheck = do
   name <- getLine
   let check = parse parserTaskName "" name
@@ -123,6 +125,14 @@ nameCheck = do
       putStrLn "Enter name again:"
       nameCheck
     Right n -> pure n
+
+dateCheck :: IO Day
+dateCheck = do 
+  dateIO <- getLine
+  today <- processCurrentDay
+  case parse (parserDueDate today) "" dateIO of  
+    Left e -> undefined
+    Right newDate -> newDate
 
 -- | processCurrentDay is used as argument for the @Parser.parserDueDate@ to get
 --   today's date in Day type format, so that we can enter a nuber of days to set
