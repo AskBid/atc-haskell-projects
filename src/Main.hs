9cccc4fa2d0f55eaf38ca23a48e9d5ffe4c960de
AskBid <noreply>
2025-04-23 11:42:07 +0100

    initiated to insert command line arguments logic

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,15 @@
 
 module Main where
 
+import System.Environment
+
 import File (readTasks)
 import Interface (loop)
 
 main :: IO ()
 main = do
+  args <- getArgs
+  putStrLn $ show args
   putStrLn "Welcome to my TODO List Manager!"
   ts <- readTasks
   loop ts
