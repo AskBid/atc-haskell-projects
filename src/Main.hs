ec2a8ef6d576ed5c69eb40e820424301fbc0ca1f
AskBid <noreply>
2025-04-24 12:21:21 +0100

    write help in command line

--- src/Main.hs
+++ src/Main.hs
@@ -32,10 +32,27 @@ add name = do
     Left _ -> putStrLn nameParserError 
     Right name -> do
       task <- mkTask name
-      putStrLn name
+      ts <- readTasks
+      pure $ sortTasks $ task:ts
+      putStrLn $ "\" " ++ name ++ "\" task was written in the database."
 
 help :: IO ()
 help = do
-  putStrLn "view"
-  putStrLn "add"
-  putStrLn "edit"
+  let textBlock = unlines [ "Usage:"
+                          , "  todo [<OPTIONS>]           Starts the CLI interactive interface" 
+                          , "  todo add <TASK_NAME>       Quickly adds a task to the locally saved todolist"
+                          , "  todo view                  Shows a list of completed and uncompleted tasks"
+                          , ""
+                          , "Available options:"
+                          , "  --help, -h                 Shows this help text"
+                          , ""
+                          , "Commands for interactive CLI interface:"
+                          , "  exit                       Exits the interactive interface"
+                          , "  add                        Dialogue to add a new todo-list-task" 
+                          , "  view                       Shows a list of all uncompleted tasks"
+                          , "  completed                  Shows a list of all completed tasks"
+                          , "  mark                       Dialogue to mark a file as completed"
+                          , "  delete                     Dialogue to delete a task"
+                          , "  edit                       Dialogue to edit an existing task"
+                          , "  help                       Shows the interactive CLI interface available actions" ]
+  putStrLn textBlock
