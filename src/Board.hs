47ef0caa109730384a55398d1308565c145729ac
AskBid <noreply>
2025-04-25 17:08:42 +0100

    mocked up logic to asses if line is winning, but is buggy

--- src/Board.hs
+++ src/Board.hs
@@ -2,19 +2,31 @@ module Board where
 
 import Data.List
 
-data Player = O | X
+data Player = O | X 
+  deriving (Show, Eq)
 
 type Board = [[Maybe Player]]
 
-win :: Board -> _
-win ps = rows . cols . diag
-
-rows :: Board -> [[Maybe Player]]
-rows rs = rs ++ transpose rs ++ diagonals rs
+winLine :: [Maybe Player] -> Maybe Player
+winLine (Nothing:_) = Nothing
+winLine (p:ps) = foldr (\p' pSoFar -> samePlayer p pSoFar) p ps
   where
+    samePlayer p' pSoFar
+      | p' == pSoFar = p'
+      | otherwise    = Nothing
+-- could make accumulator to a counter (Bool, Int) in case of `win < lineLength`
+
+winLines :: Board -> [[Maybe Player]]
+winLines rows = rows ++ columns rows ++ diagonals rows
+  where
+    columns = transpose
     diagonals = undefined
 
 -- row :: [Player] -> Bool
 -- row ps = foldr (\p (p', b) ->  ) (Player, True) ps
 
-
+-- 1[1,2,3]
+-- 2[1,2,3]
+-- 3[1,2,3]
+--
+--
