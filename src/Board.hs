5d763cfe0ed801e299e86504b023c1d08a80bee0
AskBid <noreply>
2025-04-25 15:10:25 +0100

    first logic attempt getting all possible lines for triples

--- src/Board.hs
+++ src/Board.hs
@@ -1,13 +1,20 @@
 module Board where
 
+import Data.List
+
 data Player = O | X
 
 type Board = [[Maybe Player]]
 
--- win :: Board -> Player
--- win ps = rows . cols . diag
---   where 
---     rows = row <$> ps
---     row = all 
+win :: Board -> _
+win ps = rows . cols . diag
+
+rows :: Board -> [[Maybe Player]]
+rows rs = rs ++ transpose rs ++ diagonals rs
+  where
+    diagonals = undefined
+
+-- row :: [Player] -> Bool
+-- row ps = foldr (\p (p', b) ->  ) (Player, True) ps
 
 
