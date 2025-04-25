d92bff80824e280c269201cb25f2f90a459fd697
AskBid <noreply>
2025-04-25 17:31:59 +0100

    fixed winLine bug, was probably recursive mistake, need refactor

--- src/Board.hs
+++ src/Board.hs
@@ -9,11 +9,13 @@ type Board = [[Maybe Player]]
 
 winLine :: [Maybe Player] -> Maybe Player
 winLine (Nothing:_) = Nothing
-winLine (p:ps) = foldr (\p' pSoFar -> samePlayer p pSoFar) p ps
-  where
-    samePlayer p' pSoFar
-      | p' == pSoFar = p'
-      | otherwise    = Nothing
+winLine (p:ps) = foldl (\p' pSoFar -> if p' == pSoFar then p' else Nothing) p ps
+-- winLine (p:ps) = foldl (\p' pSoFar -> samePlayer p pSoFar) p ps
+  -- where
+  --   samePlayer Nothing _ = Nothing
+  --   samePlayer _ Nothing = Nothing
+  --   samePlayer p' p''     | p' == p'' = p''
+  --                         | otherwise = Nothing
 -- could make accumulator to a counter (Bool, Int) in case of `win < lineLength`
 
 winLines :: Board -> [[Maybe Player]]
