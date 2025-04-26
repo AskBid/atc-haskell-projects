d7a48a7f25803ccd021eb116a8dba6bc82e57114
AskBid <noreply>
2025-04-26 21:11:03 +0100

    refined smalller diagonals logic

--- src/Board.hs
+++ src/Board.hs
@@ -32,38 +32,7 @@ bb2 = [[Just X, Just O, Nothing]
       ,[Just X, Nothing, Just O]
       ,[Just O, Just X, Nothing]]
 
--- calculating smaller diagonals as well.
--- [0 0, 1 1, 2 2]
--- [0 2, 1 1, 2 0]
--- [0 1, 1 0] 
--- [1 2, 2 1]
--- [0 0]
--- [2 2]
-
 bb3 = [[Just X, Just O, Nothing, Just X]
       ,[Just X, Nothing, Just O, Nothing]
       ,[Just O, Just X,  Just X, Just O]
       ,[Just O, Just X, Nothing, Just O]]
-
--- len = 3 (length - 1)
--- [0 0, 1 1, 2 2, 3 3]              0 1 2 3 -- 0 1 2 3
--- i=[0..len]                        i          i
--- [0 3, 1 2, 2 1, 3 0]              0 1 2 3 -- 3 2 1 0 
---                                   i          len - i
---
--- len = 2 (length - 1)
--- [0 2, 1 1, 2 0]                   0 1 2   -- 2 1 0
--- i=[0..len-1]                      i          len - i  
--- [1 3, 2 2, 3 1]                   1 2 3   -- 3 2 1 
---                                   i+1        len - (i - 1)
---
--- len = 1 (length - 1)
--- [0 1, 1 0]                        0 1     -- 1 0
--- i=[0..len-2]                      i          len - i       
--- [2 3, 3 2]                        2 3     -- 3 2
---                                   i+2        len - (i - 2)
--- [0 0]
--- [3 3]
---
-
-
