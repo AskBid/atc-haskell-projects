5c3a98130b0898ca1462a442289ff077b1f4b24f
AskBid <noreply>
2025-04-28 14:59:40 +0100

    refines winStreak adding comments and a type instead of the tuple for pawns streak count

--- src/Game.hs
+++ src/Game.hs
@@ -18,20 +18,28 @@ mkGame boardL ctw
   | ctw <= boardL && ctw > 1 = Just $ Game (mkBoard boardL) O ctw
   | otherwise                = Nothing
 
--- |
+-- | Find the winner in a line from the Board (from @Board.boardlines@)
+--   @Game@ argument is only used to get the @countToWin@ value that gives the
+--   lenght of tokeens required for a win. 
+--   @[Maybe Player]@ is the line being analysed to find a winner.
 -- Qs: is using Game bad for performance Vs only Int? 
 --     trade off with clarity/solidity?
 winStreak :: Game -> [Maybe Player] -> Maybe Player
 winStreak _ [] = Nothing
-winStreak game (p:ps) = if fst maxStreak >= ctw 
-                        then snd maxStreak 
-                        else Nothing
+winStreak game (p:ps) = 
+  if pawns maxStreak >= ctw 
+  then player maxStreak 
+  else Nothing
   where
     ctw = countToWin game
-    maxStreak = foldl compare (1, p) ps
-    compare (count, p) p'
-      | count >= ctw = (count, p)
-      | isNothing p' = (1, p')
-      | p' == p      = (count+1, p')
-      | otherwise    = (1, p')
+    maxStreak = foldl compare (StreakCount 1 p) ps
+    compare (StreakCount c p) p'
+      | c >= ctw = StreakCount c p
+      | isNothing p' = StreakCount 1 p'
+      | p' == p      = StreakCount (c+1) p'
+      | otherwise    = StreakCount 1 p'
 
+data StreakCount = StreakCount
+  { pawns  :: Int
+  , player :: Maybe Player
+  }
