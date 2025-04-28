075e5dc31d7c38835cebef407e42f76cd76901ae
AskBid <noreply>
2025-04-28 23:03:01 +0100

    format all rows to print for board visualisation

--- src/Graphics.hs
+++ src/Graphics.hs
@@ -18,21 +18,23 @@ import Helpers as H
 
 
 -- | prints the vertical edges of the pawns' cells, the pawn and/or empty sapces.
-pawnSpaces :: [Maybe Player] -> String
-pawnSpaces [] = "|"
-pawnSpaces (p:ps) = pawnSpace p ++ pawnSpaces ps
+pawnSpaces :: [Maybe Player] -> Int -> String
+pawnSpaces [] i = "| " ++ show i
+pawnSpaces (p:ps) i = pawnSpace p ++ pawnSpaces ps i
   where
     pawnSpace Nothing   = "|   "
     pawnSpace (Just O)  = "| O "
     pawnSpace (Just X)  = "| X "
 
 -- | prints the upper and lower edge of the pawn's cells
-edgeH :: String -> String
-edgeH s = take (length s) $ repeat '-'
+edgeH :: [Maybe Player] -> String
+edgeH []     = "-"
+edgeH (p:ps) = "----" ++ edgeH ps
 
 -- | gives and array of all the rows we need to print to visualise the board.
-boardRows :: Board -> [String]
-boardRows [] = []
-boardRows (row:rows) = edgeH cells : cells : boardRows rows ++ [edgeH cells]
-  where 
-    cells = pawnSpaces row
+boardRows :: Board -> Int -> [String]
+boardRows (row:[]) i   = edgeH row : pawnSpaces row i : [edgeH row]
+boardRows (row:rows) i = edgeH row : pawnSpaces row i : boardRows rows (i+1)
+
+hIndexs :: String
+hIndexs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
