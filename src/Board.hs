93eb07e5495c43ffe3150a0ed9515a9875adcfa5
AskBid <noreply>
2025-04-28 01:49:24 +0100

    refined comment and found typo bug with a formula n list comprension

--- src/Board.hs
+++ src/Board.hs
@@ -18,24 +18,22 @@ mkBoard n = take n $ repeat (take n $ repeat Nothing)
 --   uses diagsCycle to recursively find the diagonals at each distance from 
 --   middle diagonals.
 boardlines :: Board -> [[Maybe Player]]
-boardlines board = rows ++ columns ++ diagonals
+boardlines board = diagonals
   where
     rows = board
     columns = transpose rows
     maxDistFromCenter = (length board) - minimumWin
     diagonals = diagsCycle board maxDistFromCenter
 
+-- | given a Board-like structure - square [[]] - finds 4 diagonals lists.
+--   it takes a board structure as argument and a distance from the central diagonals.
+--   if distance is 0. Returns the middle diagonals twice therefore the @n == 0@ case.
 --         A direction             B direction
---
 --          x x 2 1 0               0 1 2 x x
 -- Upside<--x 2 1 0 1               1 0 1 2 x-->Upside
 --          2 1 0 1 2-->Downside    2 1 0 1 2
 --          1 0 1 2 x    Downside<--x 2 1 0 1
 --          0 1 2 x x               x x 2 1 0
---
--- | given a Board-like structure - [[]] - finds 4 diagonals lists.
---   it takes a board structure as argument and a distance from the central diagonals.
---   if distance is 0. Returns the middle diagonals twice therefore the @n == 0@ case.
 diagsCycle :: [[a]] -> Int -> [[a]]
 diagsCycle b n
   | n < 0     = []
@@ -46,6 +44,6 @@ diagsCycle b n
       [ b !!  i    !! (n+i),  -- A direction Upside
         b !!  i    !! (l-i),  -- B direction Upside
         b !! (i+n) !!  i,     -- A direction Downside
-        b !! (i+n) !! (l-i-n) -- B direction Downside
+        b !! (i+n) !! (l-i+n) -- B direction Downside
       ] | i <- [0..l]] ++ diagsCycle b (n-1)
     l = (length b) -1 - n
