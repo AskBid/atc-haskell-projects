ce6e056cf78020c3b6d7dd4644fee48716bcbd7d
AskBid <noreply>
2025-04-30 15:09:34 +0100

    set up for Parser

---
+++ src/Parser.hs
@@ -0,0 +1,12 @@
+module Parser where
+
+import Text.Parsec
+import Text.Parsec.String (Parser)
+
+import Board
+
+coordinateParser :: Parser Coordinate
+coordinateParser = undefined
+-- AB 1
+-- 1 AB
+-- 1 3
