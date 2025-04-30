97b6e8f8a27dfd5bd73c148f846894ecad2dcfd4
AskBid <noreply>
2025-04-30 15:03:22 +0100

    refactored cli functions in their Interface module out of Main

---
+++ src/Interface.hs
@@ -0,0 +1,28 @@
+module Interface where
+
+import System.IO (hFlush, stdout)
+
+loop :: IO ()
+loop = do
+  putStr "Enter command: "
+  hFlush stdout
+  input <- getLine
+  isLooping <- handleInput input
+  if isLooping
+    then loop
+    else return ()
+
+handleInput :: String -> IO Bool
+handleInput "exit" = do
+  putStrLn "Goodbye!"
+  pure False
+handleInput "standard" = do
+  putStrLn "Enter board size:"
+  putStrLn "Enter winning streak amount:"
+  putStrLn "Enter game type:"
+  putStrLn "1. Classic"
+  putStrLn "2. Disappearing"
+  pure True
+handleInput input = do
+  putStrLn $ "You entered: " ++ input
+  pure True
