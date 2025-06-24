15eb37667a10d130e22907d36bb55bf47d3f8f6d
AskBid <noreply>
2025-06-24 14:29:49 +0100

    tide up/move around some code

--- frontend/src/LoginPage.hs
+++ frontend/src/LoginPage.hs
@@ -19,6 +19,7 @@ import Control.Monad.Trans (lift)
 
 loginPage :: ObeliskWidget t (R FrontendRoute) m  => AppState t -> RoutedT t () m ()
 loginPage appState = do
+
   el "h2" $ text "Login here."
   el "label" $ text "Username: "
   usrEl <- inputElement $ def & initialAttributes .~ 
@@ -31,14 +32,15 @@ loginPage appState = do
   let usrDyn = _inputElement_value usrEl
   let pwdDyn = _inputElement_value pwdEl
   let logReqDyn = LoginReq <$> usrDyn <*> pwdDyn
+  let loginReqEv = tagPromptlyDyn logReqDyn submitClick
+  -- ^ loginReqEv, (postJson "text"), submitClick
+  --         ==
+  -- e l,        (l -> xhr),        e c 
+  -- :: (l -> xhr) -> e l -> e xhr 
+  -- (postJson "text") <$> e l :: e xhr 
+  -- performRequestAsync       :: e xhr -> m (e xhr) 
+   
   prerender (pure ()) $ do 
-    let loginReqEv = tagPromptlyDyn logReqDyn submitClick
-    -- ^ loginReqEv, (postJson "text"), submitClick
-    --         ==
-    -- e l,        (l -> xhr),        e c 
-    -- :: (l -> xhr) -> e l -> e xhr 
-    -- (postJson "text") <$> e l :: e xhr 
-    -- performRequestAsync       :: e xhr -> m (e xhr) 
     let url = getUrl $ FullRoute_Backend BackendRoute_Api :/ Tail_Login
     respEv <- performRequestAsync $ (postJson url) <$> loginReqEv
     setRoute $ FrontendRoute_Main :/ () <$ ffilter (statusCheck 200 300) respEv
@@ -46,5 +48,6 @@ loginPage appState = do
       "Wrong credentials. Try again." <$ ffilter (not . statusCheck 200 300) respEv
     el "h4" $ dynText message
     return ()
+   
   return ()
 
