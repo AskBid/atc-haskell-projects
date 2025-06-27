3469d0613713f54c2ab8e3dbbe04e668a51d2d66
AskBid <noreply>
2025-06-27 23:05:26 +0100

    no need of MonadJSM if using prerender

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -32,10 +32,6 @@ import Control.Monad.Trans (lift)
 import Control.Lens (Identity (..))
 import Data.Aeson (Value(..))
 import Control.Monad.IO.Class --(liftIO)
-import Language.Javascript.JSaddle (MonadJSM)
-
-
-
 
 
 import Common
@@ -95,7 +91,7 @@ frontend = Frontend
   }
 
 buttonLogInOut 
-  :: (DomBuilder t m, SetRoute t (R FrontendRoute) m, MonadJSM (Performable m), Prerender t m)
+  :: (DomBuilder t m, SetRoute t (R FrontendRoute) m, Prerender t m)
   => Bool -> RoutedT t a m ()
 buttonLogInOut logBool = 
   if not logBool 
