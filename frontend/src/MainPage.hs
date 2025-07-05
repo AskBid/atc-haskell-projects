f4f7765a39a1ff8d41c4179054f1f3f3aacb4397
AskBid <noreply>
2025-07-05 21:15:08 +0100

    first extraction of tweet text into mainpage

--- frontend/src/MainPage.hs
+++ frontend/src/MainPage.hs
@@ -12,12 +12,16 @@ import Obelisk.Route
 import Obelisk.Route.Frontend
 import Obelisk.Frontend
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
+import qualified Data.Aeson as A
+import qualified Data.ByteString.Lazy as BL
 import Data.Functor.Identity
 import Common.Api (LoginReq(..))
 -- import Control.Monad.Trans (lift)
 import Control.Monad.IO.Class (liftIO)
-
+import Schema
 import Data.Maybe
+import Database.Persist.Sql
 
 mainPage 
   :: ( ObeliskWidget t (R FrontendRoute) m)  
@@ -29,7 +33,7 @@ mainPage appState = do
   area <- textAreaElement $ def & initialAttributes .~ 
     ("placeholder" =: "Write your X here ..." <> "class" =: "bg-blue-100 w-full p-2 rounded min-h-40")
    
-  prerender (pure never) $ do
+  prerender (pure ()) $ do
     evPostBuild <- getPostBuild
     let url = getUrl $ FullRoute_Backend BackendRoute_Api :/ Api_Posts
         xhrReq = XhrRequest
@@ -39,8 +43,14 @@ mainPage appState = do
           }
     evResp <- performRequestAsync $ xhrReq <$ evPostBuild
     dRespT <- holdDyn "." $ (\r -> fromMaybe ".." $ _xhrResponse_responseText r) <$> evResp
+    let fromTtoTweets :: T.Text -> Maybe [Tweet]
+        fromTtoTweets = A.decode . BL.fromStrict . TE.encodeUtf8
+        dMtweets = fromTtoTweets <$> dRespT
+    el "h1" $ dynText $ 
+      (\twts -> tweetText $ head twts) <$> 
+      (\mtwts -> fromMaybe ([Tweet "" (Just $ toSqlKey 1) (toSqlKey 1)]) mtwts) <$> dMtweets
     el "h1" $ dynText dRespT
-    return evResp
+    return ()
 
   return ()
 
