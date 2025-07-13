0bb641f5b67faa8da2759306ba2518c746986c16
AskBid <noreply>
2025-07-13 16:12:06 +0100

    setting up cabal, imports and routes for websocket

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -1,10 +1,21 @@
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Backend where
 
 import Common.Route
 import Obelisk.Backend
+import Obelisk.Route
+import Snap
 
 backend :: Backend BackendRoute FrontendRoute
 backend = Backend
-  { _backend_run = \serve -> serve $ const $ return ()
+  { _backend_run = \serve -> serve backendHandlers
   , _backend_routeEncoder = fullRouteEncoder
   }
+
+backendHandlers :: R BackendRoute -> Snap ()
+backendHandlers = \case
+  BackendRoute_Missing :/ () -> writeBS "404"
+  BackendRoute_Websocket :/ () -> writeBS "ciao"
