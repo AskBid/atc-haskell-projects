a1d3cce666937b2898a9af19f7b5fb5eda11491b
AskBid <noreply>
2025-07-17 15:19:38 +0100

    passing around a TVar for list of connections

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -12,8 +12,10 @@ import qualified Network.WebSockets as WS
 import qualified Network.WebSockets.Snap as WSSnap
 import Data.Text 
 import Control.Monad 
-import Data.Time
-import Control.Concurrent
+import Data.Time (getCurrentTime)
+import Control.Concurrent (threadDelay)
+import Control.Concurrent.STM 
+import Control.Monad.IO.Class (liftIO)
 
 backend :: Backend BackendRoute FrontendRoute
 backend = Backend
@@ -24,15 +26,17 @@ backend = Backend
 backendHandlers :: R BackendRoute -> Snap ()
 backendHandlers = \case
   BackendRoute_Missing :/ () -> writeBS "404"
-  BackendRoute_Websocket :/ () -> WSSnap.runWebSocketsSnap wsHandler
+  BackendRoute_Websocket :/ () -> do 
+    conns <- liftIO $ atomically $ newTVar []
+    WSSnap.runWebSocketsSnap $ wsHandler conns
   -- ^ runWebSocketsSnap is just a bridge — it hands off the PendingConnection to your wsHandler. 
   -- Everything else is up to you. Broadcast messages to all clients, Count or log active connections,
   -- Assign client IDs or session tokens, Or keep chat history...
 
 -- | @type ServerApp = PendingConnection -> IO ()@ is a fucntion type, hence why `pending`
 --   appears down here.
-wsHandler :: WS.ServerApp
-wsHandler pending = do
+wsHandler :: TVar [WS.Connection] -> WS.ServerApp
+wsHandler conns pending = do
   conn <- WS.acceptRequest pending
   forever $ do
     time <- getCurrentTime
