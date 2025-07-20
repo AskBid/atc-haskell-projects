7466555e2cd4d323267a7d93f0e0fd4d74e5acd5
AskBid <noreply>
2025-07-20 15:23:43 +0100

    tweak some comments

--- src/Interface.hs
+++ src/Interface.hs
@@ -114,9 +114,9 @@ handleInput ts input = do
 
 -- | enterTaskAttributes is the part of the CLI interface used to deal with 
 --   @Task@ attributes insertion or edits. It goes through every attribute one
---   by one with the interface. Only `completed` attribute is left out as that
---   is dealt with the `mark` interface action.
---   It always takes a Task even when newly created.
+--   by one by the interface. Only the @completed@ attribute is left out as 
+--   that is dealt with the `mark` interface action.
+--   It always takes a @Task@ even when newly created.
 enterTaskAttributes :: Task -> IO Task
 enterTaskAttributes task = do 
   putStrLn ":::"
