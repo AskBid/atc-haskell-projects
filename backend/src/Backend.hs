7c01f7c62a62249ceac2277b9b0fe485bc78b2a4
AskBid <noreply>
2025-07-20 17:02:33 +0100

    clean testing lines for WS previous issue

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -29,13 +29,10 @@ backend = Backend
   }
 
 backendHandlers :: TVar [NamedConn] -> R BackendRoute -> Snap ()
-backendHandlers conns r = do
-  liftIO $ putStrLn $ "Received backend route: " <> show r
-  case r of
-    BackendRoute_Missing :/ () -> writeBS "404"
-    BackendRoute_Websocket :/ params -> do 
-      liftIO $ putStrLn "prooooovaaaaaaaaa"
-      WSSnap.runWebSocketsSnap $ wsHandler conns
+backendHandlers conns = \case
+  BackendRoute_Missing :/ () -> writeBS "404"
+  BackendRoute_Websocket :/ params -> do 
+    WSSnap.runWebSocketsSnap $ wsHandler conns
   -- ^ runWebSocketsSnap is just a bridge — it hands off the PendingConnection to your wsHandler. 
   -- Everything else is up to you. Broadcast messages to all clients, Count or log active connections,
   -- Assign client IDs or session tokens, Or keep chat history...
@@ -45,7 +42,6 @@ backendHandlers conns r = do
 wsHandler :: TVar [NamedConn] -> WS.ServerApp
 wsHandler tvConns pending = do
   let path = toString $ WS.requestPath $ WS.pendingRequest pending
-  putStrLn $ "-------------->>>>>>>>>>>> " <> path
   let req = WS.pendingRequest pending
       path = WS.requestPath req
   putStrLn $ "Request path: " <> show path
@@ -54,6 +50,6 @@ wsHandler tvConns pending = do
     time <- getCurrentTime
     let strTime = pack $ show time
     conns <- liftIO $ atomically $ readTVar tvConns
-    sequence_ $ WS.sendTextData conn <$> fst <$> conns -- [IO ()} 
+    sequence_ $ WS.sendTextData conn <$> fst <$> conns -- [IO ()] 
     WS.sendTextData conn ( strTime :: Text)
     threadDelay 1000000
