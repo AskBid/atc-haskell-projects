d2bbab2aaf0233255d65f863a04dc240600f3957
AskBid <noreply>
2025-07-24 18:12:00 +0100

    add Event to WS to send messages and frontend elements for it

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -45,20 +45,29 @@ frontend = Frontend
           dRoute <- askRoute
           el "h1" $ dynText dRoute
           prerender_ blank $ do
-
+            
+            el "lable" $ text "Connect w/ username:"
             elInp <- inputElement def
             (elBtn, _) <- el' "button" $ text "click"
+            el "br" blank
+            el "label" $ text "Message:"
+            elInpMex <- inputElement def
+            (elBtnMex, _) <- el' "button" $ text "click"
+
             let eClick = domEvent Click elBtn
+                eSend = domEvent Click elBtnMex
                 dInp = _inputElement_value elInp
+                dMex = _inputElement_value elInpMex
                 eName = tagPromptlyDyn dInp eClick
+                eMex = tagPromptlyDyn dMex eClick
                 eUrl = ("ws://localhost:8000/ws/" <>) <$> eName
             dName <- holdDyn "--" eName
             el "div" $ dynText dName
             
             let eSocket = ffor eUrl $ \url -> do 
                   liftIO $ putStrLn $ "Opening WebSocket at: " ++ T.unpack url
-                  let wsConfig :: Reflex t => WebSocketConfig t T.Text
-                      wsConfig = def & webSocketConfig_reconnect .~ False
+                  let wsConfig = def & webSocketConfig_reconnect .~ False
+                                     & webSocketConfig_send .~ ((:[]) <$> eMex)
                   ws <- webSocket url wsConfig
                   let evText = ET.decodeUtf8 <$> _webSocket_recv ws
                   -- ^ Event keep on triggering when new message comes from WS backend
