a437c28b2a6a7cdbcc9122a2765bc882ac6749f9
AskBid <noreply>
2025-07-24 14:47:37 +0100

    WebSocketConfig reconnect set to False to avoid repetitive request when no user

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -57,7 +57,9 @@ frontend = Frontend
             
             let eSocket = ffor eUrl $ \url -> do 
                   liftIO $ putStrLn $ "Opening WebSocket at: " ++ T.unpack url
-                  ws <- webSocket url (def :: Reflex t => WebSocketConfig t T.Text)
+                  let wsConfig :: Reflex t => WebSocketConfig t T.Text
+                      wsConfig = def & webSocketConfig_reconnect .~ False
+                  ws <- webSocket url wsConfig
                   let evText = ET.decodeUtf8 <$> _webSocket_recv ws
                   -- ^ Event keep on triggering when new message comes from WS backend
                   performEvent_ $ liftIO . putStrLn . T.unpack <$> evText
