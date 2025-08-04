edd95b455e867e3ad88bb575ba28c22e5a71bd47
AskBid <noreply>
2025-08-04 14:05:42 +0100

    add logout button if logged in

--- frontend/src/Common.hs
+++ frontend/src/Common.hs
@@ -5,12 +5,15 @@ module Common where
 import Data.Text
 import Reflex.Dom.Core
 import Database.Persist.Class.PersistEntity (Entity)
+import Obelisk.Route
+import Data.Functor.Identity
 
 import Common.Api
 import Schema
+import Common.Route
 
 buttonStyle :: Text  
-buttonStyle = "mt-4 bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700"
+buttonStyle = "px-4 py-3 text-sm leading-tight bg-blue-600 text-white rounded hover:bg-blue-700"
 
 inputStyle :: Text  
 inputStyle = "border border-gray-300 rounded px-3 py-2"
@@ -20,6 +23,9 @@ labelStyle = "text-sm font-medium"
 
 divVerticalStyle :: Text
 divVerticalStyle = "flex flex-col gap-4 p-4"
+
+divHorizontalStyle :: Text
+divHorizontalStyle = "flex flex-row gap-4 p-4 items-center justify-center"
 
 -- | translates websocket messages into Frontend messages, @Text@s ready for the chat.
 feMessage :: Maybe WSMessage -> Text
@@ -36,3 +42,21 @@ data AppState t = AppState
   , loggedAs :: Dynamic t (Maybe (Entity User))
   , guestAs :: Maybe Text
   }
+
+-- | @fullRouteEncoder@ has an `Either Text` as a first (check) argument
+--   to make it Identity as required from the use of @encode@, we need first to pass
+--   it under the check of @checkEncoder@ which gets rid off the uncertainty if we receive text
+--   or not.
+safeEncoder :: Encoder Identity Identity (R (FullRoute BackendRoute FrontendRoute)) PageName
+safeEncoder = 
+  case checkEncoder fullRouteEncoder of
+    Left err  -> error $ "Encoder check failed in safeEncoder: " <> unpack err
+    Right enc -> enc
+
+-- | Given a FullRoute returns a Text Url
+--  getUrl (FullRoute_Frontend (ObeliskRoute_App FrontendRoute_Login) :/ ())
+--  getUrl $ FullRoute_Backend BackendRoute_Api :/ Tail_Login
+getUrl :: R (FullRoute BackendRoute FrontendRoute) -> Text
+getUrl route = intercalate "/" $ fst pageName
+  where 
+    pageName = encode safeEncoder route
--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -4,14 +4,15 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE RecursiveDo #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Frontend where
 
-import Control.Lens ((^.))
+import Control.Lens ((^.), Identity)
 import Control.Monad
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as ET
-import Language.Javascript.JSaddle (liftJSM, js, js1, jsg)
+import Language.Javascript.JSaddle (liftJSM, js, js1, jsg, MonadJSM)
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.Aeson as A
 import qualified Data.ByteString.Lazy as BSL 
@@ -45,7 +46,9 @@ frontend = Frontend
   , _frontend_body = do
 
       ePostBuild <- getPostBuild
+
       dynLoggedUser <- holdDyn (Nothing :: Maybe (Entity User)) $ Nothing <$ ePostBuild
+
       let appState = AppState 
             { authWSconn = Nothing
             , unAuthWSconn = Nothing
@@ -55,15 +58,28 @@ frontend = Frontend
 
       elClass "div" "flex w-full h-screen" $ do
         elClass "div" "flex-1 bg-gray-200" $ do 
-          el "div" $ text "Welcome to Chat!"
+
+          elClass "div" ("bg-white " <> divHorizontalStyle) $ do
+            
+            dyn_ $ ffor (loggedAs appState) $ \case
+              Just _ -> do 
+                prerender_ blank $ do
+                  (elBtnLogout, _) <- elClass' "button" buttonStyle $ text "Logout"
+                  let eClickLogout = domEvent Click elBtnLogout 
+                  resp <- requestLogin eClickLogout
+                  blank
+              Nothing -> void $ el "div" $ text "Welcome to Chat!"
+
           subRoute_ $ \case
             FrontendRoute_Main -> mainPage appState
             FrontendRoute_User -> userChat appState
 
         elClass "div" "w-[clamp(100px,15%,999px)] bg-gray-100 gap-2 p-2" $ do 
+
           elClass "h1" "text-green-500" $ text "Connected Users"
           elClass "div" divVerticalStyle $ do 
             listUsers ["sergio", "mario", "UnAuthUser_Phill"] ePostBuild
+
           elClass "h1" "text-red-500" $ text "Offline Users"
           elClass "div" divVerticalStyle $ do
             listUsers ["bob", "alice"] ePostBuild
@@ -84,3 +100,19 @@ listUsers
 listUsers names event = do 
   dUsrList <- holdDyn [] $ names <$ event
   simpleList dUsrList (\dText -> el "div" $ dynText dText)
+
+requestLogout 
+  :: ( Monad m
+     , MonadJSM m
+     , MonadJSM (Performable m)
+     , PerformEvent t m
+     , TriggerEvent t m
+     ) 
+  => Event t a -> m (Event t XhrResponse)
+requestLogout event = 
+  let
+    url = getUrl $ FullRoute_Backend BackendRoute_Logout :/ ()
+    req = xhrRequest "POST" url $ def 
+  in do 
+    resp <- performRequestAsync $ req <$ event
+    return resp
