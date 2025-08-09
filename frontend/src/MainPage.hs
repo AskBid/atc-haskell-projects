7ab0e740538053798b3693d6e7e32f06995b333f
AskBid <noreply>
2025-08-09 22:40:50 +0100

    refactoring authentication buttons out of mainpage

--- frontend/src/MainPage.hs
+++ frontend/src/MainPage.hs
@@ -17,6 +17,7 @@ import Control.Monad (void, join)
 import qualified Data.Maybe as DM (isJust, fromMaybe)
 import Data.Map (fromList, Map)
 import Control.Monad.Fix
+import Language.Javascript.JSaddle (MonadJSM)
 
 import Common
 import Common.Api
@@ -57,10 +58,15 @@ mainPage appState = do
       elClass "label" labelStyle $ text "Connect w/ username:"
       elInpName <- inputElement $ def 
         & initialAttributes .~ ("class" =: inputStyle)
+      elClass "label" labelStyle $ text "Authenticate w/ password:"
+      elInpPwd <- inputElement $ def 
+        & initialAttributes .~ ("class" =: inputStyle <> "type" =: "password")
       let eInput = domEvent Input elInpName
+          dPwd = _inputElement_value elInpPwd
           dName = _inputElement_value elInpName 
       eDebounced <- debounce 0.8 eInput
       let eName = tagPromptlyDyn dName eDebounced
+          dCredentials = Credentials <$> dName <*> dPwd
       
       ws <- webSocket "ws://localhost:8000/ws" $ def 
               & webSocketConfig_reconnect .~ False
@@ -72,10 +78,6 @@ mainPage appState = do
             \case 
               Just (UserExist name) -> "user exist."
               otherwise             -> "no user, want to create?"
-
-      elClass "label" labelStyle $ text "Authenticate w/ password:"
-      elInpPwd <- inputElement $ def 
-        & initialAttributes .~ ("class" =: inputStyle <> "type" =: "password")
       
       let dNameLenght = ffor dName $ \name -> T.length name > 2
           dLoginConditions = fmap and $ sequence 
@@ -86,35 +88,19 @@ mainPage appState = do
             [ dNameLenght
             , not <$> (join $ ffor dWSMessage $ (wsMessage2equalName dName))
             ]
-
-      let dPwd = _inputElement_value elInpPwd
                 
       dresult <- holdDyn "Enter your credentials or input new ones to signup." eUserExistence
-      elClass "div" "flex items-center justify-center" $ dynText dresult
+      elClass "div" "flex items-center justify-center text-blue-500" $ dynText dresult
 
-      (elBtn, _) <- elDynAttr' "button" 
-        (toggleEnableStyle dLoginConditions) $ text "Connect"
-      (elBtnSignUp, _) <- elDynAttr' "button" 
-        (toggleEnableStyle dSignupConditions) $ text "Sign Up"
+      evLoginRes <- loginButton dCredentials dLoginConditions
+      
 
-      let eClick = domEvent Click elBtn
-          dCredentials = Credentials <$> dName <*> dPwd
-          eCredentials = tagPromptlyDyn dCredentials eClick
 
-          url = getUrl $ FullRoute_Backend BackendRoute_Login :/ ()
-
-          reqLogin = \credentials -> xhrRequest "POST" url $ def
-            & xhrRequestConfig_withCredentials .~ True
-            & xhrRequestConfig_headers .~ 
-              ("Authorization" =: (ET.decodeUtf8 $ BSL.toStrict $ A.encode credentials))
-
-      evRes <- performRequestAsync $ reqLogin <$> eCredentials
-
-      let evEitherResp = decodeResponse "Invalid username or password." evRes
+      let evEitherResp = decodeResponse "Invalid username or password." evLoginRes
           evErr = either id (const "Login success.") <$> evEitherResp
           evOkUsr = fmapMaybe (either (const Nothing) id) evEitherResp
       dErr <- holdDyn "" evErr
-      el "div" $ dynText $ dErr
+      elClass "div" "text-red-500" $ dynText $ dErr
       performEvent_ $ liftIO . loggedTrigger appState <$> LoggedIn <$> evOkUsr
       setRoute $ (FrontendRoute_User :/ ) <$> userName <$> evOkUsr
 
@@ -129,11 +115,59 @@ wsMessage2equalName dName wsm =
     UserExist nameuser -> (nameuser ==) <$> dName
     otherwise          -> False <$ dName
 
-toggleEnableStyle 
+enableDisable 
   :: Functor (Dynamic t)
   => Dynamic t Bool -> Dynamic t (Map T.Text T.Text)
-toggleEnableStyle dConditions = ffor dConditions $ 
+enableDisable dConditions = ffor dConditions $ 
   \enabled ->
     if enabled
       then (fromList [("class", buttonStyle)])
       else (fromList [("class", buttonStyleDisabled), ("disabled","")])
+
+loginButton 
+  :: ( Monad m
+     , DomBuilder t m
+     , PostBuild t m
+     , MonadJSM (Performable m)
+     , PerformEvent t m
+     , TriggerEvent t m
+     )
+  => Dynamic t Credentials -> Dynamic t Bool -> m (Event t XhrResponse)
+loginButton dCredentials enableCondition = do 
+  (elBtn, _) <- elDynAttr' "button" (enableDisable enableCondition) $ text "Connect"
+  let eClick = domEvent Click elBtn 
+      eCredentials = tagPromptlyDyn dCredentials eClick
+
+      url = getUrl $ FullRoute_Backend BackendRoute_Login :/ ()
+
+      reqLogin = \credentials -> xhrRequest "POST" url $ def
+        & xhrRequestConfig_withCredentials .~ True
+        & xhrRequestConfig_headers .~ 
+            ("Authorization" =: (ET.decodeUtf8 $ BSL.toStrict $ A.encode credentials))
+
+  evRes <- performRequestAsync $ reqLogin <$> eCredentials
+  return evRes
+
+signupButton 
+  :: ( Monad m
+     , DomBuilder t m
+     , PostBuild t m
+     , MonadJSM (Performable m)
+     , PerformEvent t m
+     , TriggerEvent t m
+     )
+  => Dynamic t Credentials -> Dynamic t Bool -> m (Event t XhrResponse)
+signupButton dCredentials enableCondition = do
+  (elBtn, _) <- elDynAttr' "button" (enableDisable enableCondition) $ text "Sign Up"
+  let eClick = domEvent Click elBtn 
+      eCredentials = tagPromptlyDyn dCredentials eClick
+
+      url = getUrl $ FullRoute_Backend BackendRoute_Signup :/ ()
+
+      reqLogin = \credentials -> xhrRequest "POST" url $ def
+        & xhrRequestConfig_withCredentials .~ True
+        & xhrRequestConfig_headers .~ 
+            ("Authorization" =: (ET.decodeUtf8 $ BSL.toStrict $ A.encode credentials))
+
+  evRes <- performRequestAsync $ reqLogin <$> eCredentials
+  return undefined
