51526fc947241a827d40ae4e990fb7228b7af023
AskBid <noreply>
2025-08-16 17:17:18 +0100

    add beam-automigrate dependencies

--- default.nix
+++ default.nix
@@ -6,12 +6,24 @@
 }:
 with obelisk;
 project ./. ({ pkgs, ... }: {
-  overrides = self: super: {                                     
-    beam-core = self.callHackage "beam-core" "0.9.2.1" {};
-    beam-sqlite = self.callHackage "beam-sqlite" "0.5.1.2" {};
-    beam-migrate = self.callHackage "beam-migrate" "0.5.1.2" {};
-    sqlite-simple = self.callHackage "sqlite-simple" "0.4.18.2" {};
+  overrides = self: super: {  
+    beam-core = self.callHackage "beam-core" "0.10.0.0" {};
+    beam-sqlite = self.callHackageDirect {
+      pkg = "beam-sqlite";
+      ver = "0.5.2.0";
+      sha256 = "9lWuaS4NDNB9HJdHLyU5UQrdcrXjwKc+9UPyoALBcH0=";
+    } {};
+    beam-automigrate = self.callHackageDirect {
+      pkg = "beam-automigrate";
+      ver = "0.1.7.0";
+      sha256 = "zQTdesKW82An7SYwc61SO1RJB7Vt0ZjxDvKKcxKxUtE=";
+    } {};
     aeson = self.callHackage "aeson" "2.0.3.0" {};
+    beam-migrate = self.callHackageDirect {
+      pkg = "beam-migrate";
+      ver = "0.5.2.0";
+      sha256 = "rzPgCMOfqnowdEK98HzhmBsSXh9xzB4vUG+E62v5o2k="; 
+    } {};
   };
   android.applicationId = "systems.obsidian.obelisk.examples.minimal";
   android.displayName = "Obelisk Minimal Example";
