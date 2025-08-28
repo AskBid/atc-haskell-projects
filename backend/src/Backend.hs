3e1e08ff3bb959a7fceeba0171bd004afbb82cbb
AskBid <noreply>
2025-08-28 16:29:28 +0100

    add user backend query logic to user route to start chat

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -132,18 +132,18 @@ backendHandlers conns pgConn = \case
     case mUsername of
       Nothing -> do
         modifyResponse $ setResponseStatus 401 "unauthorized"
-        writeBS "invalid JWT"
+        liftIO $ putStrLn "Not Authorised. Please login or signup."
       Just username -> do
-        modifyResponse $ setResponseStatus 200 "OK"
         users <- liftIO $ conduitQuery pgConn queryUserByName username 
         case users of
           [] -> do 
             modifyResponse $ setResponseStatus 401 "unauthorized"
-            writeBS "User did not exist."
-          (u:_) -> writeBS $ BL.toStrict $ A.encode u
+            liftIO $ putStrLn "User did not exist."
+          (u:_) -> do 
+            modifyResponse $ setResponseStatus 200 "OK"
+            writeBS $ BL.toStrict $ A.encode u
 
-  BackendRoute_Websocket :/ WebscocketRoute_User :/ user -> do
-    -- is user authenticated or visiting?
+  BackendRoute_Websocket :/ WebscocketRoute_User :/ user -> do 
     users <- liftIO $ conduitQuery pgConn queryUserByName user 
     case users of
       [] -> do
@@ -152,6 +152,21 @@ backendHandlers conns pgConn = \case
         writeBS "401 - Unauthorized"
       (u:_) -> do
         liftIO $ putStrLn "User found... going to open socket..."
+        -- is user authenticated or visiting?
+        mUsername <- verifyJWT
+        case mUsername of
+          Nothing -> do
+            modifyResponse $ setResponseStatus 401 "unauthorized"
+            writeBS "invalid JWT"
+          Just username -> do
+            users <- liftIO $ conduitQuery pgConn queryUserByName username 
+            case users of
+              [] -> do 
+                modifyResponse $ setResponseStatus 401 "unauthorized"
+                writeBS "User did not exist."
+              (u:_) -> do 
+                modifyResponse $ setResponseStatus 200 "OK"
+                writeBS $ BL.toStrict $ A.encode u
         WSSnap.runWebSocketsSnap $ wsHandler conns u
 
   BackendRoute_Websocket :/ WebscocketRoute_Main :/ () -> do 
@@ -225,7 +240,7 @@ queryUserByName :: Text -> Q P.Postgres ChatDB s (UserT (QExpr P.Postgres s))
 --   so you don’t accidentally mix rows from different queries or cross scope 
 --   boundaries incorrectly.
 --   Think of s like a unique query id at the type level.
---   Every time you start a new query (Q … s …), GHC invents a new s.
+--   Every time you start a new query (Q _ s _), GHC invents a new s.
 --   That way Beam can enforce rules like:
 --     You can join rows from the same scope (s matches).
 --     You cannot directly compare an expression from query A with query B (s ≠ s').
