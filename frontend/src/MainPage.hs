edf4692ec885f45e48644de887800f7ae77328aa
AskBid <noreply>
2025-09-09 23:13:26 +0100

    tide up logic in MainPage

--- frontend/src/MainPage.hs
+++ frontend/src/MainPage.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE GADTs               #-}
 {-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE LambdaCase           #-}
 
 module MainPage where
 
@@ -18,7 +19,7 @@ import Database.Persist.Sql
 import Data.Maybe
 import Data.Time (getCurrentTime, UTCTime(..), fromGregorian, secondsToDiffTime)
 import Control.Monad.IO.Class (liftIO, MonadIO)
--- import Control.Monad.Trans (lift)
+
 import Schema
 import Common.Api (TweetUserResp(..))
 import Common.MyFunctions (headSafe)
@@ -29,82 +30,88 @@ mainPage
   => AppState t -> RoutedT t () m ()
 mainPage appState = do
 
-  buttonLogInOut appState $ FrontendRoute_Main :/ ()
-
-  el "h2" $ text "Welcome to My X!"
-  textArea <- textAreaElement $ def & initialAttributes .~ 
-    ("placeholder" =: "Write your X here ..." 
-    <> "class" =: "bg-blue-100 w-full p-2 rounded min-h-40")
-  (elBtnPost, _) <- myButton "Post"
-  
-  let evPostClick = domEvent Click elBtnPost
-      dTextArea = _textAreaElement_value textArea
-      evPostMEUser = tagPromptlyDyn (loggedUser appState) $ evPostClick
-      evMEUserLogged = ffilter isJust evPostMEUser
-      evMEUserNotLog = ffilter (not . isJust) evPostMEUser
-      url = getUrl $ 
-        FullRoute_Backend 
-        BackendRoute_Api :/ 
-        Api_SubmitPost
-  
   prerender_ blank $ do 
 
-    let mkTweet :: Maybe (Entity User) -> T.Text -> Maybe Tweet
-        mkTweet Nothing areaText = Nothing
-        mkTweet (Just (Entity k _)) areaText = Just $ Tweet 
-          { tweetText = areaText
-          , tweetReplyTo = Nothing
-          , tweetOwner = k
-          , tweetCreatedAt = Nothing
-          }
+    buttonLogInOut appState $ FrontendRoute_Main :/ ()
 
-        
-        evMaybeTweet = flip tagPromptlyDyn evPostClick $
-          mkTweet <$> loggedUser appState <*> dTextArea
+    el "h2" $ text "Welcome to My Tweetter!"
+    textArea <- textAreaElement $ def & initialAttributes .~ 
+      ("placeholder" =: "Write your Tweet here ..." 
+      <> "class" =: "bg-blue-100 w-full p-2 rounded min-h-40")
+    (elBtnPost, _) <- myButton "Post"
+  
+    let evPostClick = domEvent Click elBtnPost
+        dTextArea = _textAreaElement_value textArea
 
-        evTweet = fmapMaybe id evMaybeTweet
+        evPostWithMEUser = tagPromptlyDyn (loggedUser appState) $ evPostClick
+        evMEUserLogged = ffilter isJust evPostWithMEUser
+        evMEUserNotLog = ffilter (not . isJust) evPostWithMEUser
 
+    setRoute $ FrontendRoute_Login :/ () <$ evMEUserNotLog
+    
+    ----------------
+    -- posting tweet
+    let evMkTweet = fmapMaybe id $ 
+          tagPromptlyDyn 
+            (mkTweet <$> loggedUser appState <*> dTextArea)
+            evPostClick
 
-        evTweetReq = ffor evTweet $ \tweet -> XhrRequest
+        xhrSubmitPost = \tweet -> XhrRequest
           { _xhrRequest_method = "POST"
-          , _xhrRequest_url = url
+          , _xhrRequest_url = getUrl $ 
+              FullRoute_Backend 
+              BackendRoute_Api :/ 
+              Api_SubmitPost
           , _xhrRequest_config = def 
               & xhrRequestConfig_withCredentials .~ True
               & xhrRequestConfig_headers .~ ("Content-Type" =: "application/json")
               & xhrRequestConfig_sendData .~ BL.toStrict (A.encode tweet)
           }
 
-    setRoute $ FrontendRoute_Login :/ () <$ evMEUserNotLog
-
-    evResp <- performRequestAsync evTweetReq
-
-    evPostBuild <- getPostBuild
-    let evReload = leftmost [evPostBuild, () <$ evResp]
-        urlPost = getUrl $ FullRoute_Backend BackendRoute_Api :/ Api_Posts
-        xhrReq = XhrRequest
-          { _xhrRequest_method = "GET"
-          , _xhrRequest_url = urlPost
-          , _xhrRequest_config = def & xhrRequestConfig_withCredentials .~ True
-          }
-    
-    evResp' <- performRequestAsync $ xhrReq <$ evReload
-    dRespT <- holdDyn "." $ (\r -> 
-      fromMaybe ".." $ _xhrResponse_responseText r) <$> evResp'
+    evTweetPostResp <- performRequestAsync $ xhrSubmitPost <$> evMkTweet
+    -- posting tweet
+    ----------------
 
     elClass "div" "text-gray-400 text-sm space-y-0" $ do 
       el "div" $ text "Click on tweet's username to see its profile." 
       el "div" $ text "Click on tweet's text to expand its reply if any."
 
     el "div" $ do
-      let fromTtoTweets :: T.Text -> Maybe TweetUserResp
-          fromTtoTweets = A.decode . BL.fromStrict . TE.encodeUtf8
-          dMtweets = fromTtoTweets <$> dRespT
-          dTUResp = fromMaybe (TweetUserResp [] []) <$> dMtweets
-      dyn_ $ ffor dTUResp elTweetsList
-      -- ^ dyn_ runs the Dynamic t (m ()), otherwise you'd only have a Dynamic not run.
+
+      evPostBuild <- getPostBuild
+
+      let evReload = leftmost [evPostBuild, () <$ evTweetPostResp]
+          -- ^ we refresh general posts feed at page start or tweet post.
+          xhrGetPosts = XhrRequest
+            { _xhrRequest_method = "GET"
+            , _xhrRequest_url = getUrl $ 
+                FullRoute_Backend 
+                BackendRoute_Api :/ 
+                Api_Posts
+            , _xhrRequest_config = def & xhrRequestConfig_withCredentials .~ True
+            }
+    
+      evGetPostsResp <- performRequestAsync $ xhrGetPosts <$ evReload
+      
+      dPosts <- holdDyn Nothing $ ffor evGetPostsResp $ \resp ->
+        case _xhrResponse_responseText resp of
+          Nothing   -> Nothing
+          Just text -> A.decode . BL.fromStrict . TE.encodeUtf8 $ text
+
+      dyn_ $ ffor dPosts $ \case
+        Nothing     -> el "div" $ text "something went wrong."
+        Just tuResp -> elTweetsList tuResp
+
       return ()
     return ()
-
   return ()
 
+mkTweet :: Maybe (Entity User) -> T.Text -> Maybe Tweet
+mkTweet Nothing _                    = Nothing
+mkTweet (Just (Entity k _)) areaText = Just $ Tweet 
+  { tweetText = areaText
+  , tweetReplyTo = Nothing
+  , tweetOwner = k
+  , tweetCreatedAt = Nothing
+  }
 
